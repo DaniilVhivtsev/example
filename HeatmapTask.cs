f2387778e627f9a38ce5f642a89ec2d05c4b1157
Даниил Павлович <danya.vshivtsev@mil.ru>
2020-11-25 22:26:01 +0500

    Добавьте файлы проекта.

---
+++ HeatmapTask.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        enum Months
+        {
+            Январь,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var sizeArray = 30;
+            var countdown = 2;
+            string[] xlabels = SetTimeString(sizeArray, countdown);
+
+            sizeArray = 12;
+            countdown = 1;
+            string[] yLabels = SetTimeString(sizeArray, countdown);
+            // yLabels[] = (Enum.GetNames(typeof(Months))).ToString();
+
+            double[,] heat = SetIntensity(names);
+
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                heat,
+                xlabels,
+                yLabels);
+        }
+
+        private static double[,] SetIntensity(NameData[] names)
+        {
+            double[,] heat = new double[30, 12];
+            
+            foreach (var person in names)
+            {
+                if (person.BirthDate.Day != 1) heat[person.BirthDate.Day - 2, person.BirthDate.Month - 1]++;
+            }
+            return heat;
+        }
+
+        private static string[] SetTimeString(int sizeArray, int countdown)
+        {
+            var xLabels = new string[sizeArray];
+
+            for (int i = countdown; i < sizeArray + countdown; i++)
+            {
+                xLabels[i - countdown] = i.ToString();
+            }
+
+            return xLabels;
+        }
+    }
+}
+
---
+++ HistogramTask.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        static int minYear;
+        static int maxYear;
+        static int periods;
+        static int firstPeriod;
+        static int lastPeriod;
+
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            SetMinMaxYear(names);
+            string[] xLabels = SetPeriodsOfYearsInString();
+
+            double[] yValues = SetNumFertilityOfName(names, name);
+
+            return new HistogramData(
+                string.Format("Рождаемость людей "),
+                xLabels,
+                yValues);
+        }
+
+        public static void SetMinMaxYear(NameData[] names)
+        {
+            minYear = int.MaxValue;
+            maxYear = int.MinValue;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].BirthDate.Year < minYear) minYear = names[i].BirthDate.Year;
+                if (names[i].BirthDate.Year > maxYear) maxYear = names[i].BirthDate.Year;
+            }
+        }
+
+        private static double[] SetNumFertilityOfName(NameData[] names, string name)
+        {
+            var yValues = new double[31];
+            foreach (var person in names)
+            {
+                var index = (person.BirthDate.Year - firstPeriod) / 10;
+                yValues[index]++;
+            }
+            return yValues;
+        }
+
+        public static string[] SetPeriodsOfYearsInString()
+        {
+            periods = ((maxYear - minYear) / 10) + 1;
+
+            var xLabels = new string[periods+1];
+
+            firstPeriod = minYear / 10 * 10;
+            lastPeriod = maxYear / 10 * 10;
+
+            var period = firstPeriod;
+            int i = 0;
+
+            while(period <= lastPeriod)
+            {
+                xLabels[i] = (period).ToString() + " - " + (period + 9).ToString();
+                i++;
+                period += 10;
+            }
+
+            return xLabels;
+        }
+    }
+}
